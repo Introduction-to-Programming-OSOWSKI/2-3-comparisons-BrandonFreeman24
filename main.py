9b310e114c41d9c48e62d5d87bc7e0762b561a5a
BrandonFreeman24 <818119@mystma.org>
2021-09-28 15:13:39 -0500

    1

--- main.py
+++ main.py
@@ -1,1 +1,31 @@
-#WRITE YOUR CODE IN THIS FILE
+def greaterThan(x, y):
+    if x > y:
+        return True
+    else:
+        return False
+
+def lessThan(x, y):
+    if x < y:
+        return True
+    else:
+        return False
+
+def equalTo(x, y):
+    if x == y:
+        return True
+    else:
+        return False
+
+def greaterOrEqul(x, y):
+    if x >= y:
+        return True
+    else:
+        return False 
+
+def lessOrEqual(x, y):
+    if x <= y:
+        return True
+    else:
+        return False
+
+print(lessOrEqual( 7, 8))
